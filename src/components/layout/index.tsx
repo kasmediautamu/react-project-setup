89f53699d5cbd4445a18dea706b10f306184bd5f
rodrickcalvin <rodrickwamala@gmail.com>
2021-07-21 10:14:07 +0300

    Adds Navigation bar to Layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,11 @@
 import React from "react"
+import Navigation from "../navigation"
 import Footer from "../footer"
 
 const Layout: React.FC = () => {
   return(
     <>
+      <Navigation/>
       <Footer/>
     </>
   )
