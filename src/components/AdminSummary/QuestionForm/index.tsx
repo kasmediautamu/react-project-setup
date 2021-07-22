501d9b06d2beab18ddcd7b6769f5f2fe0acdd10b
kasmediautamu <ivan.khabuyhe1@gmail.com>
2021-07-22 15:19:05 +0300

    Adds styles to question form

--- src/components/AdminSummary/QuestionForm/index.tsx
+++ src/components/AdminSummary/QuestionForm/index.tsx
@@ -1,5 +1,4 @@
 import "./styles.scss";
-import Hidden from "../../Hidden";
 import Input from "../../BaseForm/InputField";
 import Dropdown from "../../BaseForm/Dropdown";
 import Button from "../../BaseForm/Button";
@@ -13,14 +12,17 @@ const QuestionForm = () => {
       <form className="question-form">
         <p className="form__title">Add Question</p>
         <div className="question__detail">
-          <p>Question ID</p>
-          <p>1.0</p>
+          <p className="id__label">Question ID</p>
+          <p className="idnumber">1.0</p>
         </div>
+       
         <div className="question__detail">
-          <p>Required?</p>
+          {/* <p>Required?</p> */}
           <img src={_toggle} alt="" />
         </div>
+       
         <Input type="text" _name="question" _placeholder="Enter Question..." />
+
         <Dropdown options={[""]} />
         <div className="form__buttons">
           <Button primary="button">Save</Button>
