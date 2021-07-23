61ff77068a7b1d60a1eed7492246f194fed6ed9a
rodrickcalvin <rodrickwamala@gmail.com>
2021-07-23 16:47:05 +0300

    Updates new template page

--- src/components/SidePanel/index.tsx
+++ src/components/SidePanel/index.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react'
+import Button from "../BaseForm/Button"
+import './styles.scss'
 
 interface Props {
   children: ReactNode;
@@ -6,10 +8,14 @@ interface Props {
 }
 
 const SidePanel = ({ children, heading }: Props) => {
-  return(
+  return (
     <div className="card side-panel">
       <h3 className="side-panel__heading">{heading}</h3>
       {children}
+      <div className="actions-section">
+        <Button primary="button" btnClass="action__continue">Continue</Button>
+        <Button primary="button" btnClass="action__cancel">Cancel</Button>
+      </div>
     </div>
   )
 }
--- src/models/template.tsx
+++ src/models/template.tsx
@@ -1,8 +1,11 @@
 export interface TEMPLATE {
-    id:number
-    name:string
-    version:string
-    type:string
-    status:string
-    date:string
+    id: number
+    name: string
+    version: string
+    type: string
+    status: string
+    date: string
+    lineOfBusiness: string
+    group: string
+    fontStyle: string
 }
